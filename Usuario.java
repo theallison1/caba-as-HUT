73746ff3190bcaa188460b5b47f04c6990a9d9ee
theallison1 <34327988+theallison1@users.noreply.github.com>
2017-12-07 20:43:46 +0100

    Add files via upload
    archivos .java van dentro del src del proyecto

---
+++ Usuario.java
@@ -0,0 +1,93 @@
+
+
+package modelo;
+
+/**
+ 
+ */
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String mail;
+    private String telefono;
+    private String tipo_usuario;
+    private String nom_usuario;
+    private String pass;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo_usuario() {
+        return tipo_usuario;
+    }
+
+    public void setTipo_usuario(String tipo_usuario) {
+        this.tipo_usuario = tipo_usuario;
+    }
+
+    public String getNom_usuario() {
+        return nom_usuario;
+    }
+
+    public void setNom_usuario(String nom_usuario) {
+        this.nom_usuario = nom_usuario;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+ 
+    
+}
